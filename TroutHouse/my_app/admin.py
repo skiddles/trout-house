be3f00d649282f6915e8282cb488525b7d312991
skiddles <marks.technical@outlook.com>
2018-12-06 07:06:16 -0600

    End of Django Tutorials

--- TroutHouse/my_app/admin.py
+++ TroutHouse/my_app/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("This is the custom text question header set in my_app.admin.py",  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
